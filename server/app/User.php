aca122013e78e1408079957371330d054c7be21f
takumi_kurogi <takumines1@gmail.com>
2020-10-28 19:16:22 +0900

    [update]モデル修正

--- server/app/User.php
+++ server/app/User.php
@@ -5,9 +5,11 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Scout\Searchable;
 
 class User extends Authenticatable
 {
+    use Searchable;
     use Notifiable;
 
     /**
@@ -36,4 +38,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function resolveChildRouteBinding($childType, $value, $field)
+    {
+        // TODO: Implement resolveChildRouteBinding() method.
+    }
+
+    public function post()
+    {
+        $this->hasOne('App\Post');
+    }
 }
