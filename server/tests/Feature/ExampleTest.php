0a4a72305593f1dfee985c663f6325a7a410834c
takumi_kurogi <takumines1@gmail.com>
2020-10-31 00:06:58 +0900

    [update]アルゴリズムテスト

--- server/tests/Feature/ExampleTest.php
+++ server/tests/Feature/ExampleTest.php
@@ -14,8 +14,26 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $nums = [2,7,11,15];
+        $target = 9;
+        $result = $this->twoSum($nums, $target);
 
-        $response->assertStatus(200);
+        $this->assertEquals($result, [0,1]);
+    }
+
+    function twoSum($nums, $target) {
+
+        asort($nums);
+        $i = 0;
+        $int = count($nums);
+        foreach ($nums as $num) {
+            for ($n = $i + 1 ;$n < $int ;$n++) {
+                if ($nums[$i] + $nums[$n] === $target) {
+                    return $answer = array($i, $n);
+                }
+            }
+            unset($nums[$i]);
+            $i++;
+        }
     }
 }
